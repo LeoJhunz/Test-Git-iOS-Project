c038a768138f7e16c7a447e28948c10aa50da7aa
Leo Cawit Jr <leojhunz@Leos-Mac.local>
2014-08-04 01:23:56 +0800

    added a test class

---
+++ Git Test/PMTest.h
@@ -0,0 +1,13 @@
+//
+//  PMTest.h
+//  Git Test
+//
+//  Created by Leo Cawit Jr. on 8/4/14.
+//  Copyright (c) 2014 Primal Marketing LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PMTest : NSObject
+
+@end
---
+++ Git Test/PMTest.m
@@ -0,0 +1,13 @@
+//
+//  PMTest.m
+//  Git Test
+//
+//  Created by Leo Cawit Jr. on 8/4/14.
+//  Copyright (c) 2014 Primal Marketing LLC. All rights reserved.
+//
+
+#import "PMTest.h"
+
+@implementation PMTest
+
+@end
